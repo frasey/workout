07dde74a5bdfb40e878399ac5471e59f9689a316
Eilein <eilein_fraser@hotmail.co.uk>
2023-09-15 17:52:43 +0100

    controller to add new exercise to database

--- controllers/exercise_controller.py
+++ controllers/exercise_controller.py
@@ -6,6 +6,11 @@ from models.workout import Workout
 exercise_blueprint = Blueprint("/exercise", __name__)
 
 # show all exercises
+@exercise_blueprint.route("/exercise")
+def show_all():
+    exercises = Exercise.query.all()
+    workouts = Workout.query.all()
+    return render_template("exercises/add_new.jinja", exercises=exercises, workouts=workouts)
 
 # edit exercises
 
@@ -14,11 +19,14 @@ exercise_blueprint = Blueprint("/exercise", __name__)
 # delete exercise
 
 # add exercise
-# @exercise_blueprint.route("/exercise/new", methods=["POST"])
-# def add_exercise():
 
-@exercise_blueprint.route("/exercise")
-def show_all():
-    exercises = Exercise.query.all()
-    workouts = Workout.query.all()
-    return render_template("exercises/add_new.jinja", exercises=exercises, workouts=workouts)
+@exercise_blueprint.route("/exercise/new", methods=['POST'])
+def add_new_exercise():
+    name = request.form["name"]
+    sets = request.form["sets"]
+    reps = request.form["reps"]
+    new_exercise = Exercise(name=name, sets=sets, reps=reps)
+
+    db.session.add(new_exercise)
+    db.session.commit()
+    return redirect("/exercise")
