b9da8fd17e35ec5ad49d7ce4f21f12f5d96d972c
Eilein <eilein_fraser@hotmail.co.uk>
2023-09-19 14:54:56 +0100

    add/show user rewards

--- controllers/rewards.py
+++ controllers/rewards.py
@@ -1,5 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
 from app import db
 from models.rewards import Reward
 
 rewards_blueprint = Blueprint("/rewards", __name__)
+
+@rewards_blueprint.route("/rewards")
+def show_rewards():
+    rewards = Reward.query.all()
+    return render_template("rewards/add_show_rewards.jinja", rewards=rewards)
+
+@rewards_blueprint.route("/rewards", methods=['POST'])
+def add_reward():
+    name = request.form["reward"]
+    new_reward = Reward(name=name)
+    db.session.add(new_reward)
+    db.session.commit()
+
+    return redirect("/rewards")
--- models/rewards.py
+++ models/rewards.py
@@ -2,5 +2,5 @@ from app import db
 
 class Reward(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reward = db.Column(db.String(64))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    name = db.Column(db.String(64))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), default=1)
